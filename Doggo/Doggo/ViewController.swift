8db83314706a6adf1949656fff3704685020ff33
Anna Opoka <anna.opoka.external@ecb.europa.eu>
2020-08-03 22:36:50 +0200

    Initial commit

---
+++ Doggo/Doggo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Doggo
+//
+//  Created by Anna Opoka on 03.08.20.
+//  Copyright © 2020 Anna Opoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
